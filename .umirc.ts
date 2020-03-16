5e7272f502834b605c915b5c3e094f323c29c8c3
maomao1996 <1714487678@qq.com>
2020-03-16 19:32:56 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'dumi'
+
+export default defineConfig({
+  title: 'react-use-hooks',
+  // more config: https://d.umijs.org/config
+})
---
+++ src/index.md
@@ -0,0 +1,3 @@
+# react-use-hooks
+
+记录一些常用的 hooks
