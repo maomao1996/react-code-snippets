26a5d9a01b26f714a0551eb6052d2a7c4d3b1124
maomao1996 <1714487678@qq.com>
2020-04-25 22:53:01 +0800

    build: 升级依赖，修改配置项

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'dumi'
 
 export default defineConfig({
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+      }
+    ]
+  ],
+  // mode: 'site',
+  base: '/react-code-snippets/',
+  publicPath: '/react-code-snippets/',
   title: 'react-code-snippets',
+  dynamicImport: {},
+  hash: true,
+  manifest: {}
   // more config: https://d.umijs.org/config
 })
