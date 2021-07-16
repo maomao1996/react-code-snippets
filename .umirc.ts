ef111dbf9b944452c5d7030276eabc8da756d887
maomao1996 <1714487678@qq.com>
2021-07-16 17:14:46 +0800

    chore: 修改 logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'dumi'
 // more config: https://d.umijs.org/config
 export default defineConfig({
   title: 'React 学习笔记',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/favicon.ico',
+  logo: '/logo.png',
   locales: [['zh-CN', '中文']]
 })
